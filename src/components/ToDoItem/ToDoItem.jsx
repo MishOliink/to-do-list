8feb6f9d891d24d0edd45f1906d83c3a98d17012
MishOliink <MishOliink@gmail.com>
2024-01-31 21:23:20 +0200

    icons added

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -5,13 +5,16 @@ import React from 'react';
 import styles from './ToDoItem.module.css';
 
 // Icons components
-import { RiTodoFill } from 'react-icons/ri';
+import { RiTodoFill, RiDeleteBin2Line } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
 
 const ToDoItem = ({ item, deleteItem }) => {
   return (
     <div className={styles.toDoItem} onDoubleClick={() => deleteItem(item.id)}>
       <RiTodoFill className={styles.itemIcon} />
       <div className={styles.item}>{item.item}</div>
+      <RiDeleteBin2Line className={styles.deleteIcon} />
+      <FaCheck className={styles.checkIcon} />
     </div>
   );
 };
